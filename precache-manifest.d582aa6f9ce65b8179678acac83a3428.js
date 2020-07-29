bb786d4bbc4094fe4436be492ebfca5cc88cf348
Alperen Yildiz <alperenyildiz21@gmail.com>
2020-07-29 04:17:54 -0400

    Updates

---
+++ precache-manifest.d582aa6f9ce65b8179678acac83a3428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bace915f4c5a16b249ba71c75861cc4f",
+    "url": "/mucyildiz/battleship.git/index.html"
+  },
+  {
+    "revision": "71467697ded2013e0817",
+    "url": "/mucyildiz/battleship.git/static/css/main.cd1a7e6a.chunk.css"
+  },
+  {
+    "revision": "3e4f3edf3be880746987",
+    "url": "/mucyildiz/battleship.git/static/js/2.421461c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mucyildiz/battleship.git/static/js/2.421461c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71467697ded2013e0817",
+    "url": "/mucyildiz/battleship.git/static/js/main.ed2e8127.chunk.js"
+  },
+  {
+    "revision": "d085d39f8378cc97a64f",
+    "url": "/mucyildiz/battleship.git/static/js/runtime-main.9bc61acd.js"
+  }
+]);
